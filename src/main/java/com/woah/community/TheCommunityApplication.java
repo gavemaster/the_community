1f6b5d1bf898a1f4643e40af63f256999612ee03
gavemaster <zacharyringhoff@gmail.com>
2024-01-12 08:13:53 -0500

    first commit

---
+++ src/main/java/com/woah/community/TheCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.woah.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woah/community/TheCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woah.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
